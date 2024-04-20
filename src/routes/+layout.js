cc9165fc40f5374d38a0c8690392c6e6ea6945cb
Nick Vincent <the.nick.vincent@gmail.com>
2024-04-20 16:09:48 -0700

    Add initial page transitions

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,1 +1,5 @@
 export const trailingSlash = 'always';
+
+export const load = async ({ url: { pathname } }) => {
+	return { pathname };
+};
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,17 @@
 <script>
 	import Header from './Header.svelte';
+	import PageTransition from '$lib/PageTransition.svelte';
 	import './styles.css';
+
+	export let data;
+	$: pathParts = data.pathname.split('/');
+	$: dir = `/${pathParts[1]}`;
 </script>
 
 <Header />
 
 <main>
-	<slot />
+	<PageTransition key={dir}>
+		<slot />
+	</PageTransition>
 </main>
