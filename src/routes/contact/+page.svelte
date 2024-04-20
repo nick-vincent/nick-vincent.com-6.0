b362e1362e61ee7518c3c96237c4aa902e7d7b97
Nick Vincent <the.nick.vincent@gmail.com>
2024-04-20 15:23:04 -0700

    Add contact info

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -5,3 +5,9 @@
 
 <h1>Contact</h1>
 <p>Connect or get in touch.</p>
+<ul>
+	<li><a href="mailto:the.nick.vincent@gmail.com" target="_blank">Email</a></li>
+	<li><a href="https://www.linkedin.com/in/nickvincent/" target="_blank">LinkedIn</a></li>
+	<li><a href="https://www.instagram.com/the.nick.vincent/" target="_blank">Instagram</a></li>
+	<li><a href="https://www.youtube.com/@nick-vincent" target="_blank">YouTube</a></li>
+</ul>
