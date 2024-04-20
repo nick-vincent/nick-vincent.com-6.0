e1533b717876f7e0bb229177a317d89bc8a67641
Nick Vincent <the.nick.vincent@gmail.com>
2024-04-20 14:51:20 -0700

    Add layout, header, nav, & pages

---
+++ src/routes/Header.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { page } from '$app/stores';
+
+	const navItems = [
+		{ text: 'Work', href: '/work' },
+		{ text: 'Tunes', href: '/tunes' },
+		{ text: 'Faces', href: '/faces' },
+		{ text: 'Photos', href: '/photos' },
+		{ text: 'Contact', href: '/contact' }
+	];
+</script>
+
+<header>
+	<nav>
+		<a aria-current={$page.url.pathname === '/' ? 'page' : null} href="/">Nick Vincent</a>
+		<ul>
+			{#each navItems as { href, text }, i}
+				<li>
+					<a aria-current={$page.url.pathname === href ? 'page' : null} {href}>{text}</a>
+				</li>
+			{/each}
+		</ul>
+	</nav>
+</header>
