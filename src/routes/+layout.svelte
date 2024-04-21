3d1190e186897342878e6ef49042e7ec1c888967
Nick Vincent <the.nick.vincent@gmail.com>
2024-04-20 18:22:03 -0700

    Try different page & nav transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,39 @@
 <script>
 	import Header from './Header.svelte';
-	import PageTransition from '$lib/PageTransition.svelte';
-	import './styles.css';
+	import '../styles/index.css';
 
-	export let data;
-	$: pathParts = data.pathname.split('/');
-	$: dir = `/${pathParts[1]}`;
+	let navOpen = false;
 </script>
 
-<Header />
+<Header bind:navOpen />
 
-<main>
-	<PageTransition key={dir}>
+<main class:navOpen>
+	<div class="content">
 		<slot />
-	</PageTransition>
+	</div>
 </main>
+
+<style>
+	main {
+		inset: 0;
+		overflow: auto;
+		position: fixed;
+		transition-property: opacity, transform;
+		transition-duration: var(--duration-nav);
+		transition-delay: var(--duration-nav);
+		transition-timing-function: var(--ease-accelerate);
+	}
+
+	main.navOpen {
+		pointer-events: none;
+		opacity: 0;
+		transform: scale(0.95);
+		transition-delay: 0ms;
+		transition-timing-function: var(--ease-decelerate);
+	}
+
+	.content {
+		max-width: 54rem;
+		margin: 2rem;
+	}
+</style>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,7 +1,10 @@
 <script>
 	import { page } from '$app/stores';
 
+	export let navOpen = false;
+
 	const navItems = [
+		{ text: 'Home', href: '/' },
 		{ text: 'Work', href: '/work/' },
 		{ text: 'Tunes', href: '/tunes/' },
 		{ text: 'Faces', href: '/faces/' },
@@ -11,14 +14,73 @@
 </script>
 
 <header>
-	<nav>
-		<a aria-current={$page.url.pathname === '/' ? 'page' : null} href="/">Nick Vincent</a>
+	<button on:click={() => (navOpen = !navOpen)}>Menu</button>
+	<nav class:navOpen>
 		<ul>
-			{#each navItems as { href, text }, i}
+			{#each navItems as { href, text }}
 				<li>
-					<a aria-current={$page.url.pathname === href ? 'page' : null} {href}>{text}</a>
+					<a
+						{href}
+						tabindex={navOpen ? null : '-1'}
+						on:click={() => (navOpen = false)}
+						aria-current={$page.url.pathname === href ? 'page' : null}>{text}</a
+					>
 				</li>
 			{/each}
 		</ul>
 	</nav>
 </header>
+
+<style>
+	button {
+		z-index: 2;
+		cursor: pointer;
+		position: fixed;
+		top: 0;
+		left: 0;
+	}
+
+	nav {
+		z-index: 1;
+		text-align: center;
+		pointer-events: none;
+		position: fixed;
+		display: flex;
+		inset: 0;
+		opacity: 0;
+		transform: scale(0.95);
+		transition-property: opacity, transform;
+		transition-duration: var(--duration-nav);
+		transition-timing-function: var(--ease-accelerate);
+	}
+
+	nav.navOpen {
+		pointer-events: auto;
+		opacity: 1;
+		transform: scale(1);
+		transition-delay: var(--duration-nav);
+		transition-timing-function: var(--ease-decelerate);
+	}
+
+	ul {
+		padding: 0;
+		margin: auto;
+		display: block;
+	}
+
+	li {
+		padding: 0;
+		margin: 0;
+		display: block;
+	}
+
+	a {
+		display: block;
+		font-weight: 500;
+		font-size: 1.5em;
+		line-height: 1;
+		text-decoration: none;
+		padding: 0.5em 1em;
+		color: black;
+	}
+</style>
