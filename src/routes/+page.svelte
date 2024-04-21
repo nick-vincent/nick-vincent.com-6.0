e0a1ff302b34a2d756ac8fe5f7cc8b204b1f98ee
Nick Vincent <the.nick.vincent@gmail.com>
2024-04-20 21:28:31 -0700

    Add photos to home & contact pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+	import photo from '$lib/images/home.jpg';
+</script>
+
 <svelte:head>
 	<title>Nick Vincent</title>
 	<meta
@@ -7,17 +11,25 @@
 </svelte:head>
 
 <div class="wrapper">
+	<img alt="Nick recording guitar tracks." src={photo} />
 	<h1>Nick Vincent</h1>
 	<p>
 		Known to <a href="/work/">push pixels</a>, <a href="/tunes/">strum tunes</a>,
-		<a href="/faces/">make faces</a>,
-		<a href="/photos/">snap photos</a>, &
-		<a href="/contact/">reply promptly</a>.
+		<a href="/faces/">make faces</a>, &
+		<a href="/photos/">snap photos</a>.
 	</p>
 </div>
 
 <style>
 	.wrapper {
 		margin: auto;
+	}
+
+	img {
+		display: block;
+		width: 100%;
+		border-radius: 1rem;
+		margin: 0 0 0.5rem 0;
+		box-shadow: var(--image-shadow);
 	}
 </style>
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,13 +1,30 @@
+<script>
+	import photo from '$lib/images/contact.jpg';
+</script>
+
 <svelte:head>
 	<title>Nick Vincent → Contact</title>
 	<meta name="description" content="Connect or get in touch." />
 </svelte:head>
 
-<h1>Contact</h1>
-<p>Connect or get in touch.</p>
-<ul>
-	<li><a href="mailto:the.nick.vincent@gmail.com" target="_blank">Email</a></li>
-	<li><a href="https://www.linkedin.com/in/nickvincent/" target="_blank">LinkedIn</a></li>
-	<li><a href="https://www.instagram.com/the.nick.vincent/" target="_blank">Instagram</a></li>
-	<li><a href="https://www.youtube.com/@nick-vincent" target="_blank">YouTube</a></li>
-</ul>
+<div class="wrapper">
+	<img alt="Phone booth relic." src={photo} />
+	<h1>Contact</h1>
+	<p>Connect or get in touch.</p>
+	<ul>
+		<li><a href="mailto:the.nick.vincent@gmail.com" target="_blank">Email</a></li>
+		<li><a href="https://www.linkedin.com/in/nickvincent/" target="_blank">LinkedIn</a></li>
+		<li><a href="https://www.instagram.com/the.nick.vincent/" target="_blank">Instagram</a></li>
+		<li><a href="https://www.youtube.com/@nick-vincent" target="_blank">YouTube</a></li>
+	</ul>
+</div>
+
+<style>
+	img {
+		display: block;
+		width: 100%;
+		border-radius: 1rem;
+		margin: 0 0 0.5rem 0;
+		box-shadow: var(--image-shadow);
+	}
+</style>
