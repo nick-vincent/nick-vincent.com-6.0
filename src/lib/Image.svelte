0da3cd024383b9ae860bd4add7b54ee3f34f3d49
Nick Vincent <the.nick.vincent@gmail.com>
2024-04-22 18:46:36 -0700

    Fix tooltip / image layering

--- src/lib/Image.svelte
+++ src/lib/Image.svelte
@@ -12,62 +12,17 @@
 
 <style>
 	.wrapper {
-		z-index: 0;
-		position: relative;
 		border-radius: var(--border-radius);
 		overflow: hidden;
 		background-color: var(--color-image-background);
 		box-shadow: var(--image-shadow);
-		/* background-image: linear-gradient(to bottom, var(--dark-color-line), var(--light-color-line)); */
+		background-image: linear-gradient(to bottom, var(--dark-color-line), var(--light-color-line));
 	}
 
-	/* .wrapper::before {
-		position: absolute;
-		z-index: 1;
-		content: '';
-		border-radius: var(--border-radius);
-		inset: 0;
-		background-image: linear-gradient(
-			to right,
-			transparent 0,
-			var(--color-line) 100dvw,
-			var(--color-line) 200dvw,
-			transparent 300dvw
-		);
-		background-size: 300dvw 100%;
-		background-repeat: no-repeat;
-		animation-name: image-loading;
-		animation-duration: 2s;
-		animation-iteration-count: infinite;
-		animation-timing-function: linear;
-	} */
-
-	/* .wrapper::after {
-		position: absolute;
-		z-index: 3;
-		content: '';
-		border-radius: var(--border-radius);
-		inset: 0;
-		background-image: linear-gradient(to bottom, white, black);
-		background-clip: padding-box;
-		border: solid 0.2rem transparent;
-	} */
-
 	img {
-		z-index: 2;
-		position: relative;
 		display: block;
 		width: 100%;
 		height: auto;
 		margin: 0;
 	}
-
-	/* @keyframes image-loading {
-		0% {
-			background-position: -300dvw 0;
-		}
-		100% {
-			background-position: 300dvw 0;
-		}
-	} */
 </style>
